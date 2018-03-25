8b72656275ca18ae20f69174a2c86a5cc7e784d1
ThunderBall64 <33467576+ThunderBall64@users.noreply.github.com>
2018-03-25 17:47:49 -0400

    Add files via upload

---
+++ ListsDiskDrives/ListsDiskDrives/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListsDiskDrives
+{
+    class Data
+    {
+        private string fileName = "n\a";
+        private byte fileSize = 0;
+        private string fileDetails = "n\a";
+
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+        public byte FileSize
+        {
+            get { return fileSize; }
+            set { fileSize = value; }
+        }
+        public string FileDetails
+        {
+            get { return fileDetails; }
+            set { fileDetails = value; }
+        }
+
+        public override string ToString()
+        {
+            string message = "";
+            message = message + "FileName " + FileName + "\n";
+            message = message + "FileSize " + FileSize + "\n";
+            message = message + "FileDetails " + FileDetails + "\n";
+            return message;
+        }
+    }
+}
---
+++ ListsDiskDrives/ListsDiskDrives/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO; //Input:Output
+
+// List size of each disk drive
+namespace ListsDiskDrives
+{
+    class Program
+    {
+        public static void Main()
+        {
+            FileInfo info = new FileInfo("C:\\");
+            FileAttributes attributes = info.Attributes;
+            Console.WriteLine("Nature(Attribute) of the File : {0}", attributes);
+
+            Data data = new Data();
+            data.FileDetails = "Examine Files";
+            data.FileSize = 0;
+            data.FileName = "Enter File Name";
+
+            if (data.FileSize > 0)
+            {
+                Console.WriteLine("File is empty");
+            }
+            Console.WriteLine("Analyzing File Data...");
+            Console.ReadLine();
+            
+
+            DriveInfo[] listdiskdrivedetails = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in listdiskdrivedetails)
+            {
+                Console.WriteLine("Drive (0)", drive.Name);
+                Console.WriteLine("File type: {0}", drive.DriveType);
+                if (drive.IsReady == true)
+                {
+                    Console.WriteLine("Total size of drive:{0, 15} bytes ", drive.TotalSize);
+                    Console.ReadLine();
+                }
+
+            }
+            // Display the Information of the File and Disk Drive
+            // View details in console application
+            Console.ReadLine();
+        }
+    }
+}
